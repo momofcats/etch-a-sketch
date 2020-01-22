d9909b18fab3f9271bf6a24a44a3f686e2e97fcc
momofcats <ykhilko@gmail.com>
2020-01-22 12:42:04 -0800

    change cell to square size

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 const container = document.querySelector(".js-grid-container");
 addElement();
 function addElement() {
-  for (i = 0; i < 255; i++) {
+  for (i = 0; i < 256; i++) {
     const newDiv = document.createElement("div");
-    newDiv.classList.add("border", "grid__item");
+    newDiv.classList.add("outline", "grid__item");
     container.appendChild(newDiv);
   }
 }
