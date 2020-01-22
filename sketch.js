56a02b7a6c8bd48c61bc19ed47ea8db61a35ab5c
momofcats <ykhilko@gmail.com>
2020-01-22 11:23:06 -0800

    create grid of 16x16 squares

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 const container = document.querySelector(".js-grid-container");
 addElement();
 function addElement() {
-  for (i = 0; i < 256; i++) {
+  for (i = 0; i < 255; i++) {
     const newDiv = document.createElement("div");
-    newDiv.classList.add("border");
+    newDiv.classList.add("border", "grid__item");
     container.appendChild(newDiv);
   }
 }
