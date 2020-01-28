44c4a480bb041058912e43ce6fee39791ef0dc15
momofcats <ykhilko@gmail.com>
2020-01-28 15:24:16 -0800

    fixed according to comment

--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@
   const container = document.querySelector(".js-grid-container");
   const resizeButton = document.querySelector(".js-resize-grid");
   const radioButton = document.querySelector(".js-radio-buttons");
-  let colorMode = 0;
+  let colorMode = "black";
 
-  addElement(16);
+  buildGrid(16);
 
   radioButton.addEventListener("change", function(e) {
     colorMode = e.target.value;
@@ -14,28 +14,24 @@
 
   resizeButton.addEventListener("click", function(e) {
     const cellNumber = window.prompt("Enter a number 1-100?");
-    removeGrid();
-    addElement(cellNumber);
+    if (cellNumber <= 0 || cellNumber > 100) {
+      alert("Wrong number, please, try again!");
+      cellNumber = 16;
+    }
+    container.innerHTML = "";
+    buildGrid(cellNumber);
   });
 
-  function addElement(n) {
+  function buildGrid(n) {
     for (i = 0; i < n; i++) {
       const row = document.createElement("div");
       row.classList.add("grid__row", "js-grid-row", "js-grid-item");
-      container.appendChild(row);
       for (j = 0; j < n; j++) {
         const cell = document.createElement("div");
         cell.classList.add("grid__cell", "js-grid-item", "js-grid-cell");
         row.appendChild(cell);
       }
-    }
-    addDataAttribute();
-  }
-
-  function removeGrid() {
-    const grid_item = document.querySelectorAll(".js-grid-item");
-    for (i = 0; i < grid_item.length; i++) {
-      grid_item[i].remove();
+      container.appendChild(row);
     }
   }
 
@@ -43,18 +39,18 @@
     const red = Math.floor(Math.random() * 255);
     const green = Math.floor(Math.random() * 255);
     const blue = Math.floor(Math.random() * 255);
-    const rgb = "rgb(" + red + "," + green + "," + blue + ")";
+    const rgb = `rgb(${red},${green},${blue})`;
     element.style.background = rgb;
   }
 
   function changeGradient(element) {
     let oldLightness = element.dataset.lightness;
-    console.log(oldLightness);
+    if (oldLightness === undefined) {
+      oldLightness = 100;
+    }
     let newLightness = oldLightness - 10;
-    console.log(newLightness);
     element.dataset.lightness = newLightness;
-    const hsl = "hsl(" + 0 + "," + 0 + "%," + newLightness + "%)";
-    console.log(hsl);
+    const hsl = `hsl(0,0%,${newLightness}%)`;
     element.style.background = hsl;
   }
 
@@ -63,19 +59,12 @@
     if (!el.classList.contains("js-grid-cell")) {
       return;
     }
-    if (colorMode === "black" || colorMode === 0) {
+    if (colorMode === "black") {
       el.style.background = "black";
     } else if (colorMode === "random") {
       generateRGB(el);
     } else if (colorMode === "gradient") {
       changeGradient(el);
-    }
-  }
-
-  function addDataAttribute() {
-    const cell = document.querySelectorAll(".js-grid-cell");
-    for (i = 0; i < cell.length; i++) {
-      cell[i].setAttribute("data-lightness", 100);
     }
   }
 })();
